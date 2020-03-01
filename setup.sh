5dd2ef0f2d5c694b6d54978dff05cf4feea78d8e
jacobguinther <jacobgguinther@gmail.com>
2020-03-01 06:55:17 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,9 @@ xcode-select --install
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby \
+  -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+  </dev/null
 fi
 
 # Update homebrew recipes
