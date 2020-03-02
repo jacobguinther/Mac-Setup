5b56302269d4bf1ddb570cb65e49afa5d745006e
jacobguinther <jacobgguinther@gmail.com>
2020-03-01 18:26:56 -0700

    add some finder settings

--- mac-settings.sh
+++ mac-settings.sh
@@ -1,2 +1,11 @@
 echo "Setting some Mac settings..."
-# @TODO Finder Window
+
+# Finder
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file:///Users/jgguinther/Downloads/"
+
+killall Finder
+
+
+echo "Done!"
