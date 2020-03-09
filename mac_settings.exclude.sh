38aa09770b2316f335a8b562c9561f5683e82c98
jacobguinther <jacobgguinther@gmail.com>
2020-03-08 20:10:21 -0600

    add come mac default settings

--- mac_settings.exclude.sh
+++ mac_settings.exclude.sh
@@ -5,7 +5,15 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file:///Users/jgguinther/Downloads/"
 
-killall Finder
+# Menu Bar
+defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d  h:mm a"
 
+# Dock Settings
+defaults write com.apple.dock tilesize -int 43
+defaults write com.apple.dock orientation -string "left"
+
+killall SystemUIServer
+killall Finder
+killall Dock
 
 echo "Done!"
