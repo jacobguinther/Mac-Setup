ce473442fbf9c896a214b4dbace7d56eb86ce744
jacobguinther <jacobgguinther@gmail.com>
2020-03-14 11:04:38 -0600

    update mac settings and save iterm2 plist

--- mac_settings.exclude.sh
+++ mac_settings.exclude.sh
@@ -6,11 +6,23 @@ defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file:///Users/jgguinther/Downloads/"
 
 # Menu Bar
+# Hide Keyboard Input Source ( American Flag )
+defaults write com.apple.TextInputMenu visible -int 0
+# Show Date
 defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d  h:mm a"
+# Show Bluetooth
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -int 1
+defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Clock.menu" "/System/Library/CoreServices/Menu Extras/Displays.menu" "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"
 
 # Dock Settings
+# Size
 defaults write com.apple.dock tilesize -int 43
+# Position Dock Left
 defaults write com.apple.dock orientation -string "left"
+
+# ScreenCapture
+mkdir ~/Downloads/ScreenCapture
+defaults write com.apple.screencapture location -string "~/Downloads/ScreenCapture"
 
 killall SystemUIServer
 killall Finder
